dcc45e952e48f323acb5a6d89f8a0c2700813d64
Angel S. Moreno <angelxmoreno@gmail.com>
2021-04-08 00:58:18 -0400

    reverting db connection

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,16 @@ import 'reflect-metadata';
 import server from './server';
 import Env from './utils/Env';
 import {logger} from "./utils/Logger";
-import dbConnection from "./database/dbConnection";
+// import dbConnection from "./database/dbConnection";
 
 const PORT = Env.getInteger('PORT', 3000);
 const BASE_URL = Env.getString('BASE_URL', 'http://127.0.0.1');
 
-dbConnection.connect().then(() => {
-    logger.info(`Database connection established`, {label: '⚡ database'});
+// dbConnection.connect().then(() => {
+//     logger.info(`Database connection established`, {label: '⚡ database'});
     server.listen(PORT, () => {
         logger.info(`HTTP server is running at ${BASE_URL}:${PORT}`, {label: '⚡️server'});
     });
-})
+// })
 
 
