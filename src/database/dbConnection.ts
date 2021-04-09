4ecf6df7df3b3e6688c8ad520c2a2cbd73848724
Angel S. Moreno <angelxmoreno@gmail.com>
2021-04-08 20:35:56 -0400

    added naming strategy for database

--- src/database/dbConnection.ts
+++ src/database/dbConnection.ts
@@ -1,12 +1,17 @@
 import {getConnectionManager} from "typeorm";
 import Env from "../utils/Env";
+import CakePhpNamingStrategy from "./CakePhpNamingStrategy";
 
 const connectionManager = getConnectionManager();
 const dbConnection = connectionManager.create({
     type: "mysql",
     url: Env.getString('DATABASE_URL'),
     charset: 'utf8mb4',
-    timezone: 'Z'
+    timezone: 'Z',
+    namingStrategy: new CakePhpNamingStrategy(),
+    entities: [`${__dirname}/entities/*.{js,ts}`],
+    synchronize: Env.getBool('DATABASE_SYNCING', true),
+    logging: Env.getBool('DATABASE_LOGGING', true),
 });
 
 export default dbConnection;
