1508700ce52d129dfe5e7474884d76456fc2747f
Angel S. Moreno <angelxmoreno@gmail.com>
2021-04-11 13:17:46 -0400

    integrated user identity to api controller

--- src/controllers/ApiController.ts
+++ src/controllers/ApiController.ts
@@ -4,32 +4,40 @@ import {RequestWithId} from "../middlewares/requestId";
 import {toArabic, toRoman} from 'roman-numerals'
 import AnonymousUserEntity from "../database/entities/AnonymousUserEntity";
 import {NextFunction, Response} from "express";
+import UserEntity from "../database/entities/UserEntity";
 
 @JsonController('/api/v1')
 @UseBefore(async (request: RequestWithId, response: Response, next: NextFunction): Promise<void> => {
-    if (!(await AnonymousUserEntity.canRequestAccess(request))) {
+    const user = await UserEntity.getByRequest(request);
+    if (user) {
+        response.locals.user = user;
+        response.locals.userType = 'user';
+    } else if (!(await AnonymousUserEntity.canRequestAccess(request))) {
         const error = new HttpError(401, 'You have reached your limit. Please register and use an api key.');
         return next(error);
+    } else {
+        response.locals.user = await AnonymousUserEntity.createFromRequest(request);
+        response.locals.userType = 'anonymous';
     }
-    response.locals.user = await AnonymousUserEntity.createFromRequest(request);
     next();
 })
-
 export default class ApiController {
     @Get('/to-roman')
-    async toRoman(@Req() req: RequestWithId, @Res() rep: Response, @QueryParam('number') decimalNumber: number) {
-        requestLogger(req, `Converting ${decimalNumber} to decimal`);
+    toRoman(@Req() req: RequestWithId, @Res() rep: Response, @QueryParam('number', {required: true}) decimalNumber: number) {
+        const {user, userType} = rep.locals
+        requestLogger(req, `Converting ${decimalNumber} to decimal for ${userType}`, 'info', {user});
 
         const romanNumber = toRoman(decimalNumber);
-        return {romanNumber, decimalNumber, user: rep.locals.user}
+        return {romanNumber, decimalNumber, userType};
     }
 
     @Get('/to-decimal')
-    toDecimal(@Req() req: RequestWithId, @QueryParam('number') romanNumberStr: string) {
+    toDecimal(@Req() req: RequestWithId, @Res() rep: Response, @QueryParam('number', {required: true}) romanNumberStr: string) {
         const romanNumber = romanNumberStr.toUpperCase();
-        requestLogger(req, `Converting ${romanNumber} to roman`);
+        const {user, userType} = rep.locals
+        requestLogger(req, `Converting ${romanNumber} to roman for ${userType}`, 'info', {user});
 
         const decimalNumber = toArabic(romanNumber);
-        return {romanNumber, decimalNumber}
+        return {romanNumber, decimalNumber, userType};
     }
 }
